fcfae13f207f3ae5f4056cae09b090d3b3bcd148
Yung Byun <yungcheolbyun@gmail.com>
2017-10-02 17:02:37 +0900

    updated

--- 01.py
+++ 01.py
@@ -1,33 +1,24 @@
 # 1입력 1뉴런, 데이터 1개
-
 import tensorflow as tf
-import lib.myplot as myplot
 
 x = [1]
 y = [1]
 
 w = tf.Variable(tf.random_normal([1]))
-b = tf.Variable(tf.random_normal([1]))
-hypo = w * x + b
+hypo = w * x
 
 cost = (hypo - y) * (hypo - y)
 
-train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost_fun)
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-weights = []
-biases = []
 costs = []
 
 for i in range(1001):
     sess.run(train)
     if i % 50 == 0:
-        weights.append(sess.run(w))
-        biases.append(sess.run(b))
-        costs.append(sess.run(cost_fun))
+        print(sess.run(w), sess.run(cost))
+        costs.append(sess.run(cost))
 
-
-gildong = pl.MyPlot()
-gildong.show_list(costs)
