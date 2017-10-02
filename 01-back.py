ed775845c6291cc975bc6404fd9dcae8f46b7b1c
Yung Byun <yungbyun@Yungui-MacBook-Air.local>
2017-10-02 21:12:26 +0900

    updated

--- 01-back.py
+++ 01-back.py
@@ -1,14 +1,12 @@
-# 1입력 1뉴런, 학습데이터 1개, 바이어스
+# 1입력 1뉴런, 학습데이터 1개
 import tensorflow as tf
-from plot import MyPlot
 
 x = [1]
 y = [1]
 
 #----- a neuron
 w = tf.Variable(tf.random_normal([1]))
-b = tf.Variable(tf.random_normal([1]))
-hypo = w * x + b
+hypo = w * x
 #-----
 
 cost = (hypo - y) * (hypo - y)
@@ -18,15 +16,8 @@ train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-costs = []
-
 for i in range(1001):
     sess.run(train)
 
     if i % 50 == 0:
-        print(sess.run(w), sess.run(b), sess.run(cost))
-        costs.append(sess.run(cost))
-
-gildong = MyPlot()
-gildong.show_list(costs)
-
+        print(sess.run(w), sess.run(cost))
