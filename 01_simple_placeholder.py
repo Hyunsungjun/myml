78a8f48d8b4ed87cadeda244d575353efea95499
Yung Byun <yungcheolbyun@gmail.com>
2017-10-03 17:09:21 +0900

    updated

--- 01_simple_placeholder.py
+++ 01_simple_placeholder.py
@@ -4,12 +4,15 @@ import tensorflow as tf
 x = [1]
 y = [1]
 
+x_p = tf.placeholder(tf.float32)
+y_p = tf.placeholder(tf.float32)
+
 #----- a neuron
 w = tf.Variable(tf.random_normal([1]))
-hypo = w * x
+hypo = w * x_p
 
 #----- learning
-cost = (hypo - y) * (hypo - y)
+cost = (hypo - y_p) * (hypo - y_p)
 
 train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
@@ -17,11 +20,11 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for i in range(1001):
-    sess.run(train)
+    sess.run(train, feed_dict={x_p:x, y_p:y})
 
     if i % 100 == 0:
-        print('w:', sess.run(w), 'cost:', sess.run(cost))
+        print(sess.run(w), sess.run(cost, feed_dict={x_p:x, y_p:y}))
 
 #----- testing(prediction)
-print(sess.run(w * [5]))
+print(sess.run(hypo, feed_dict={x_p:[5]}))
 
